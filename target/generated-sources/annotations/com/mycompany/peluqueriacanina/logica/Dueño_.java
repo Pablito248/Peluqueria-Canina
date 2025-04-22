794595f6a43fb6bcfbbac24647da2e51a3641cdc
Pablito248 <ajuanpa995@gmail.com>
2025-04-22 16:54:36 -0500

    Finalizado parcialmente

---
+++ target/generated-sources/annotations/com/mycompany/peluqueriacanina/logica/Dueño_.java
@@ -0,0 +1,15 @@
+package com.mycompany.peluqueriacanina.logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-04-08T16:28:08", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Dueño.class)
+public class Dueño_ { 
+
+    public static volatile SingularAttribute<Dueño, Integer> id_dueño;
+    public static volatile SingularAttribute<Dueño, String> celDueño;
+    public static volatile SingularAttribute<Dueño, String> nombre;
+
+}
